9d3a8e126f0609934d49ec2a634ef92222530013
Chris Bloom <bloomcb@gmail.com>
2012-01-15 16:54:18 -0800

    semantic progress

--- slides/class-3/ai-class.js
+++ slides/class-3/ai-class.js
@@ -1,7 +1,12 @@
+function blink(){
+    $('#semantic-blink img').delay(100).fadeTo(100,0).delay(100).fadeTo(100,1, blink);
+}
 $(function() {
 	// Deck initialization
 	$.deck('.slide');
 
 	$("pre.htmlCode").snippet("html",{style:"acid"});
+
+	blink();
 });
 
