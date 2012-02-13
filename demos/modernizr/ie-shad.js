0b57eba42af4f271429c3aac4474f6bffaf02b6c
Chris Bloom <bloomcb@gmail.com>
2012-02-13 15:21:21 -0800

    text shadow working in ie7

--- demos/modernizr/ie-shad.js
+++ demos/modernizr/ie-shad.js
@@ -1,5 +1,5 @@
-if (!Modernizr.textshadow) {
   // [default] reading the current style automatically
+  /*
   $('h1').textshadow();
 
   // creates the HTML structure but doesn't try to apply the styles
@@ -16,6 +16,8 @@ if (!Modernizr.textshadow) {
 
   // hsla
   $('h1').textshadow('2px 2px 2px hsla(0, 100%, 54%, .5)');
+  */
 
   alert('ieshad got loaded');
-}
+
+  $('li').textshadow('10px 10px 5px #ff00ff');
