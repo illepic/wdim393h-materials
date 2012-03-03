08688ea4c531a79b0950ff40c66e8b3c57f43015
Chris Bloom <bloomcb@gmail.com>
2012-03-03 00:53:48 -0800

    pixel buffers

--- slides/class-9/ai-class.js
+++ slides/class-9/ai-class.js
@@ -8,23 +8,19 @@ $(function() {
   //pixel-buffers
   var img = new Image();
 	$(img).load(function(){
-		//draw the image to the canvas
-		var canvas = $('#pixel-buffers-ex canvas');
+		var canvas = $('#pixel-buffers-ex canvas'); //draw the image to the canvas
 		var c = canvas.get(0).getContext('2d');
 		c.drawImage(img,0,0); //draw image 1
-		c.drawImage(img,500,0); //draw image 2
-		//get the canvas data
-		var data = c.getImageData(500,0,img.width,img.height);
+		var data = c.getImageData(0,0,img.width,img.height); //get the canvas data
 		//invert each pixel
 		for(n=0; n<data.width*data.height; n++) {
-			var index = n*4;
+			var index = n*4; //jumps to next point in array after 4 colors
 			data.data[index]   = 255-data.data[index];
 			data.data[index+1] = 255-data.data[index+1];
 			data.data[index+2] = 255-data.data[index+2];
 			//don't touch the alpha
 		}
-		//set the recolored data back
-		c.putImageData(data,500,0);
+		c.putImageData(data,500,0); //set the recolored data back
 	});
 	img.src = "images/fire.jpg";
 });
